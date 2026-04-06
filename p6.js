bd94f6870e5d47a796210aefefbf3582cbd00317
Manas <manasdevtrivedi@gmail.com>
2026-04-06 21:37:51 +0530

    Added

--- p6.js
+++ p6.js
@@ -12,3 +12,11 @@ function deleteLast(){
     display.value = display.value.slice(0, -1);
 }
 
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = "Error";
+    }
+}
+
